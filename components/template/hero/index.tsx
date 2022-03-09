b9e37f4d048a7b0bb5d0fee640168ffbeae9e0db
rizukikhair4658 <rizkikhair4658@gmail.com>
2022-03-10 00:53:26 +0700

    feat: mobile navigation

--- components/template/hero/index.tsx
+++ components/template/hero/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import { socmed } from '../../../constants'
 
 import Button from '../../common/button'
@@ -6,38 +7,48 @@ import Button from '../../common/button'
 const Hero: React.FC = ({ ...props }) => {
   return (
     <section id="hero" className="relative max-w-screen-2xl mx-auto md:pt-24">
+      <div
+        className="fixed md:hidden left-4 top-4 bg-primary-gradient rounded-lg h-10 w-10 flex items-center justify-center lg:shadow-lg shadow-gray-800 hover:shadow-none hover:opacity-80 transition-all ease-out duration-300">
+        <Link href="/">
+          <a
+            className="font-audiowide bg-transparent text-3xl"
+            title="Rizki Khair">
+            R
+          </a>
+        </Link>
+      </div>
       <div className="px-8 min-h-screen lg:min-h-0 hero-bg flex items-center justify-center md:block md:py-24">
         <div className="h-96 pt-10 md:pt-0">
-        <div className="space-y-6 md:text-left text-center">
-          <h1 className="md:text-3xl lg:text-4xl text-2xl font-poppins font-semibold text-gray-300">
-            Hey, I'm <span className="font-bold font-montserrat text-white">Rizki</span>
-          </h1>
-          <h2 className="md:text-4xl lg:text-5xl text-3xl font-poppins font-extrabold text-white">
-            FrontEnd Developer
-          </h2>
-        </div>
-        <p className="md:text-left text-center mt-4 mb-10 text-gray-300">
-          Stick around to know more about me.
-        </p>
-        <p className="mb-10 lg:mb-20 md:text-left text-center">
-          <Button
-            link="#about"
-            customClass="px-12 py-4 rounded-full font-bold h-auto bg-primary-gradient hover:opacity-60">
-            Get Started
-          </Button>
-        </p>
-        <div className="flex justify-center md:justify-start items-center gap-x-1">
-          {
-            socmed.map((item, key) => (
-              <Button
-                key={key}
-                icon={item.name}
-                title={item.title}
-                link={item.link}
-                customClass="w-7 h-7" />
-            ))
-          }
-        </div>
+          <div className="space-y-6 md:text-left text-center">
+            <h1 className="md:text-3xl lg:text-4xl text-2xl font-poppins font-semibold text-gray-300">
+              Hey, I'm <span className="font-bold font-montserrat text-white">Rizki</span>
+            </h1>
+            <h2 className="md:text-4xl lg:text-5xl text-3xl font-poppins font-extrabold text-white">
+              FrontEnd Developer
+            </h2>
+          </div>
+          <p className="md:text-left text-center mt-4 mb-10 text-gray-300">
+            Stick around to know more about me.
+          </p>
+          <p className="mb-10 lg:mb-20 md:text-left text-center">
+            <Button
+              link="#about"
+              customClass="px-12 py-4 rounded-full font-bold h-auto bg-primary-gradient hover:opacity-60">
+              Get Started
+            </Button>
+          </p>
+          <div className="flex justify-center md:justify-start items-center gap-x-1">
+            {
+              socmed.map((item, key) => (
+                <Button
+                  key={key}
+                  icon={item.name}
+                  title={item.title}
+                  link={item.link}
+                  customClass="w-7 h-7" />
+              ))
+            }
+          </div>
         </div>
       </div>
     </section>
--- components/template/navbar/index.tsx
+++ components/template/navbar/index.tsx
@@ -8,8 +8,8 @@ import Button from '../../common/button'
 
 export default function Navbar() {
   return (
-    <header className="md:fixed z-30 top-0 left-0 right-0 overflow-hidden ">
-      <div className="max-w-screen-2xl mx-auto px-8 py-2.5">
+    <header className="navbar fixed z-30 overflow-hidden">
+      <div className="hidden md:block max-w-screen-2xl mx-auto px-8 py-2.5">
         <div className="hidden md:flex items-center justify-between gap-x-6 w-full">
           <div
             className="bg-primary-gradient rounded-lg h-10 w-10 flex items-center justify-center lg:shadow-lg shadow-gray-800 hover:shadow-none hover:opacity-80 transition-all ease-out duration-300">
@@ -31,6 +31,23 @@ export default function Navbar() {
                     customClass="highlight-gradient font-poppins text-gray-300 hover:text-white">
                     {item.name}
                   </Button>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+      <div className="block md:hidden bg-primary-dark border-t border-gray-400 border-opacity-25">
+        <div className="max-w-screen-2xl mx-auto">
+          <ul className="flex w-full items-center">
+            {
+              navigations.map((item: Navigation, key: number) => (
+                <li key={key} className="w-full">
+                  <Button
+                    icon={item.name.toLowerCase()}
+                    link={item.target}
+                    title={item.name}
+                    customClass="w-full py-2 h-16" />
                 </li>
               ))
             }
