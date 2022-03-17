37b6837f6a2fa89684e003f3d07b10e62239adc2
rizukikhair4657 <rizkikhair4658@gmail.com>
2022-03-17 13:31:14 +0700

    fix: url site for meta tag data

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 		includePaths: [path.join(__dirname, 'styles')]
 	},
 	env: {
-		url: process.env.HOST
+		url: process.env.NODE_ENV === 'development' ? 'https://rizkikhair.netlify.app' : 'https://rizkikhair.netlify.app'
 	}
 }
 
